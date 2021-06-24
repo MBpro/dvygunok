9005119f94f9598482c7d0bb946ffb107093c219
mbpro <651717@gmail.com>
2021-06-24 10:14:02 +0200

    Added button scroll to top

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,14 @@
 
-@@include('testWebp.js');
-@@include('burger.js');
+@@include('testWebp.js')
+@@include('swiper-bundle-min.js')
+@@include('swiperActivation.js')
+@@include('burger.js')
+@@include('popup.js')
+@@include('question-form.js')
+@@include('scroll-to-top.js')
+
+
+
+
+
 
